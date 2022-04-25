d9e8565476ecd5b5309233022a731ee1a0fca678
Anuj Lahoty <anujlahoty1997@gmail.com>
2022-04-25 13:26:17 +0530

    Added records to models and customized Admin panel

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment
 
-# Register your models here.
+
+# admin.register() decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'content')
+    list_filter = ('category', )
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
+
+admin.site.site_header = "Product Review Admin"
